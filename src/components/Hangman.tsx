74eec87f3b213bd1d48bfceaaf1844484078bb82
Amir Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-07-12 13:29:06 +0200

    5- Check the guessed letter

--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -4,6 +4,8 @@ import { alphabets } from "../constants/constants";
 
 const Hangman = () => {
   const [word, setWord] = useState("");
+  const [corrects, setCorrects] = useState<string[]>([]);
+  const [fails, setFails] = useState<string[]>([]);
 
   const reset = () => {
     setWord(randomizeWord());
@@ -16,11 +18,25 @@ const Hangman = () => {
     .map(() => "_")
     .join(" ");
 
+  const onGuess = (letter: string) => {
+    if (word.includes(letter)) {
+      setCorrects([...corrects, letter]);
+    } else {
+      setFails([...fails, letter]);
+    }
+  };
+
   return (
     <div>
       <p className="mask">{maskWord}</p>
       {alphabets.map((letter, index) => (
-        <button key={index}>{letter}</button>
+        <button
+          disabled={corrects.includes(letter) || fails.includes(letter)}
+          onClick={() => onGuess(letter)}
+          key={index}
+        >
+          {letter}
+        </button>
       ))}
     </div>
   );
--- src/helpers/helper.test.ts
+++ src/helpers/helper.test.ts
@@ -1,5 +1,10 @@
-describe("it should pass", () => {
-  it("will pass", () => {
-    expect(true).toStrictEqual(true);
+import { randomizeWord } from "./helper";
+
+describe("randomize word", () => {
+  it("shall return a word", () => {
+    const result = randomizeWord();
+    expect(result).toBeDefined();
+    expect(typeof result).toBe("string");
+    expect(result.length).toBeGreaterThan(0);
   });
 });
